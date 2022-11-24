7d6d44578850210dddef511230f024aa7bdcf813
Sergey Khaykov <ssserg3000@gmail.com>
2022-11-24 21:13:18 +0200

    initial commit

--- src/main/java/lang/print/gaps/task5/NumberDivider.java
+++ src/main/java/lang/print/gaps/task5/NumberDivider.java
@@ -2,5 +2,12 @@ package lang.print.gaps.task5;
 
 public class NumberDivider {
     void divide(int toBeDivided) {
+        System.out.println(((float)toBeDivided/5));
+
+    }
+
+    public static void main(String[] args) {
+        NumberDivider nd = new NumberDivider();
+        nd.divide(14);
     }
 }
--- src/main/java/lang/print/gaps/task5/TimeConvertor.java
+++ src/main/java/lang/print/gaps/task5/TimeConvertor.java
@@ -2,5 +2,13 @@ package lang.print.gaps.task5;
 
 public class TimeConvertor {
     public void convert(float minutes) {
+
+        float seconds = minutes*60;
+        System.out.println(minutes+" minutes is equal to "+seconds+" seconds");
+    }
+
+    public static void main(String[] args) {
+        TimeConvertor tc = new TimeConvertor();
+        tc.convert(50);
     }
 }
